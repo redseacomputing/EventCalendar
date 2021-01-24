ad1975bfc6004440b200ee048a963eedd2e7b16f
christopher <christopher.zelezny@mailbox.org>
2021-01-24 11:42:03 +0100

    Persistence interface defined

---
+++ src/main/java/com/sportradar/intern/persistence/DataEventAccess.java
@@ -0,0 +1,10 @@
+package com.sportradar.intern.persistence;
+
+import com.sportradar.intern.dto.Event;
+
+import java.util.List;
+
+public interface DataEventAccess {
+
+    public void getAllEvents();
+}
