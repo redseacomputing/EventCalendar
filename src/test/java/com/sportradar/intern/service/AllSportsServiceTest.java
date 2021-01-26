532c2ce360870c34412c24c9125e90847caf3a66
christopher <christopher.zelezny@mailbox.org>
2021-01-26 18:56:14 +0100

    All sports test unit

---
+++ src/test/java/com/sportradar/intern/service/AllSportsServiceTest.java
@@ -0,0 +1,15 @@
+package com.sportradar.intern.service;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AllSportsServiceTest {
+
+    @BeforeEach
+    void instantiateAllSportsServiceClass() {
+        AllSportsService allSports = new AllSportsService();
+    }
+
+
+}
