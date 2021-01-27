a42e2404776770c459750d2befda21a2ab2d5112
christopher <christopher.zelezny@mailbox.org>
2021-01-27 14:25:14 +0100

    Football Service connected with endpoint

---
+++ src/main/java/com/sportradar/intern/service/FootballService.java
@@ -0,0 +1,33 @@
+package com.sportradar.intern.service;
+
+import com.sportradar.intern.dto.Event;
+import com.sportradar.intern.dto.Team;
+import com.sportradar.intern.persistence.db.H2Category;
+import com.sportradar.intern.persistence.db.H2Event;
+import com.sportradar.intern.persistence.db.H2Team;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FootballService {
+
+    H2Event eventController;
+    H2Team teamController;
+    List<Event> events;
+
+    public List<Event> getAllEvents(){
+        eventController = new H2Event();
+        teamController = new H2Team();
+        events = new ArrayList<>();
+        List<Event> simpleEvents = eventController.fetchAllFromCategory("Soccer");
+        for (int i = 0; i < simpleEvents.size(); i++) {
+            List<Team> teamsOfEvent = teamController.fetchTeamNamesFrom(simpleEvents.get(i).getId());
+            Event event = new Event();
+            event.setId(simpleEvents.get(i).getId());
+            event.setDateOfEvent(simpleEvents.get(i).getDateOfEvent());
+            event.setTeams(teamsOfEvent);
+            events.add(event);
+        }
+        return events;
+    }
+}
